093b06941390fb5bf80b265452ef6f91c6b31edb
DevHeart1 <obasipeace412@yahoo.com>
2024-03-06 13:25:05 +0000

    Update: cleared white space

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    This is a module that queries the reddit API 
+    This is a module that queries the reddit API
     and returns the number of subscribers of a given subreddit
 """
 import requests
