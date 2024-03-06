2f1617c77444cb962ce3676abd35433feca35eb1
DevHeart1 <obasipeace412@yahoo.com>
2024-03-06 13:13:52 +0000

    Update: script for getting num. of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,18 +8,22 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-        This method returns the number of subscribers
-        args: 
-            subreddit: the name of the subreddit to query
-        return:
-            The number of subscribers or 0 on failure.  
+    This method returns the number of subscribers
+    args:
+        subreddit: the name of the subreddit to query
+    return:
+        The number of subscribers or 0 on failure.
     """
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Request from Peace(API advanced Project)"}
-    response = request.get(url, headers == headers, allow_redirects=False)
+    headers = {"User-Agent": "Request from Peace (API advanced Project)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        return response.json().get("data").get("subscibers")
+        data = response.json().get("data")
+        if data is not None and "subscribers" in data:
+            return data["subscribers"]
+        else:
+            return 0
     else:
         return 0
